fbde798433c76bc99dc2ec0dc84f21351ae40dfa
HafizhAuliansyah <muhammad.hafizh.tif21@polban.ac.id>
2022-10-23 10:35:35 +0700

    Instalasi datatable

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Yajra Data Table
+        Yajra\DataTables\DataTablesServiceProvider::class,
     ],
 
     /*
